302cf3381f4c1430c696f0208f03b66d852ae28c
yogitaborole <yogita1097@gmail.com>
2020-12-12 20:54:50 +0530

    demo

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mysql = require('mysql2')
+const cors=require('cors')
+const app = express()
+app.use(cors('*'))
+app.get('/product', (request, response) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'manager',
+        database: 'test',
+        port: 3306
+    })
+
+    connection.query('select * from product', (error, products) => {
+        connection.close()
+        response.send({
+            status: 'success',
+            data: products
+        })
+    })
+})
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('server started on port 3000')
+})
